8e541382fa26c99d4f6c270d5cca92897de31fe1
Nis Sarup <nis@sarup.dk>
2012-12-05 10:40:36 +0100

    Made a GUI to determine values for HSV color seperation for the two circles.

--- main.cpp
+++ main.cpp
@@ -12,23 +12,110 @@
 using namespace std;
 using namespace cv;
 
-void find_marker(string file);
+// Types
+typedef float T;
+typedef cv::Vec<T,3> VecT; // 3-vector
+typedef cv::Mat_<VecT> ImgT; // 3-channel image
+typedef cv::Mat_<T> MatT; // 1-channel container
+
+void animate_one();
+void find_marker_from_gray_image(string file);
+void find_marker_in_HSV_image(string file);
+void hsv_color_seperator_gui(string file);
 
 int main(int argc, const char * argv[])
 {
+//    animate_one();
+    hsv_color_seperator_gui("/Users/tamen/Documents/Archive/Skole/SDU/7Semester/VIS/Projects/Project 2/vis1_pro2_sequences/sequence3/8.png");
+    std::cout << "Done!" << endl;
+    return 0;
+}
+
+void hsv_color_seperator_gui(string file) {
+    ImgT img = cv::imread(file);
+    img /= 255;
+    
+    const int rows = img.rows;
+    const int cols = img.cols;
+    
+    ImgT imghsv;
+    cv::cvtColor(img, imghsv, CV_BGR2HSV);
+    
+    ImgT img1;
+    
+    int low = 0;
+    int high = 360;
+    int tollow = 0;
+    int tolhigh = 100;
+    
+    
+    namedWindow("Output", CV_WINDOW_NORMAL);
+    createTrackbar("Low", "Output", &low, 360, NULL);
+    createTrackbar("High", "Output", &high, 360, NULL);
+    createTrackbar("TolLow", "Output", &tollow, 100, NULL);
+    createTrackbar("TolHigh", "Output", &tolhigh, 100, NULL);
+    
+    while (true) {
+        img1 = imghsv.clone();
+        for(int i = 0; i < rows; ++i) {
+            for(int j = 0; j < cols; ++j) {
+                VecT& hsv = img1(i,j);
+                
+                const T& saturation = hsv[1];
+                if (saturation < (double)(tollow / 100.0) || saturation > (double)(tolhigh / 100.0)) {
+                    hsv = VecT(0,0,0);
+                    continue;
+                }
+                
+                const T& hue = hsv[0];
+                if (hue < low || hue > high) {
+                    hsv = VecT(0,0,0);
+                }
+            }
+        }
+        
+        cv::cvtColor(img1, img1, CV_HSV2BGR);
+        cv::imshow("Output", img1);
+        
+        cout << "High: " << getTrackbarPos("High", "Output") << endl;
+        cout << "Low: " << getTrackbarPos("Low", "Output") << endl;
+        cout << "High tol: " << (double)getTrackbarPos("TolHigh", "Output") / 100.0 << endl;
+        cout << "Low tol: " << (double)getTrackbarPos("TolLow", "Output") / 100.0 << endl;
+        
+        int c = cvWaitKey(20);
+        if((char)c==27)
+            break;
+    }
+
+}
+
+void find_marker_in_HSV_image(string file) {
+    
+    // Values found using hsv_color_seperator_gui()
+    // For the big circle:
+//        High: 198
+//        Low: 157
+//        High tol: 0.52
+//        Low tol: 0.36
+    // For the small circle:
+//        High: 344
+//        Low: 314
+//        High tol: 0.66
+//        Low tol: 0.46
+}
+
+void animate_one() {
     string folder = "/Users/tamen/Documents/Archive/Skole/SDU/7Semester/VIS/Projects/Project 2/vis1_pro2_sequences/sequence3/";
     
     for (int i = 1; i <= 30; i++) {
         stringstream out;
         out << folder << i << ".png";
-        find_marker(out.str());
+        find_marker_from_gray_image(out.str());
     }
     waitKey();
-    std::cout << "Done!" << endl;
-    return 0;
 }
 
-void find_marker(string file) {
+void find_marker_from_gray_image(string file) {
     // Load image
     Mat org = imread(file, 1);
     Mat img = imread(file, 0);
