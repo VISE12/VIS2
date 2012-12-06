6e0b99476cad47c2fd09f95bcd82bb689acd9f39
Nis Sarup <nis@sarup.dk>
2012-12-06 15:20:10 +0100

    Updated this project a bit so it only does one of the assignments. Cleaned and commented the code.

--- main.cpp
+++ main.cpp
@@ -20,88 +20,13 @@ typedef cv::Mat_<T> MatT; // 1-channel container
 
 void animate_one();
 void find_marker_from_gray_image(string file);
-void find_marker_in_HSV_image(string file);
-void hsv_color_seperator_gui(string file);
 
 int main(int argc, const char * argv[])
 {
-//    animate_one();
-    hsv_color_seperator_gui("/Users/tamen/Documents/Archive/Skole/SDU/7Semester/VIS/Projects/Project 2/vis1_pro2_sequences/sequence3/8.png");
+    animate_one();
+    waitKey();
     std::cout << "Done!" << endl;
     return 0;
-}
-
-void hsv_color_seperator_gui(string file) {
-    ImgT img = cv::imread(file);
-    img /= 255;
-    
-    const int rows = img.rows;
-    const int cols = img.cols;
-    
-    ImgT imghsv;
-    cv::cvtColor(img, imghsv, CV_BGR2HSV);
-    
-    ImgT img1;
-    
-    int low = 0;
-    int high = 360;
-    int tollow = 0;
-    int tolhigh = 100;
-    
-    
-    namedWindow("Output", CV_WINDOW_NORMAL);
-    createTrackbar("Low", "Output", &low, 360, NULL);
-    createTrackbar("High", "Output", &high, 360, NULL);
-    createTrackbar("TolLow", "Output", &tollow, 100, NULL);
-    createTrackbar("TolHigh", "Output", &tolhigh, 100, NULL);
-    
-    while (true) {
-        img1 = imghsv.clone();
-        for(int i = 0; i < rows; ++i) {
-            for(int j = 0; j < cols; ++j) {
-                VecT& hsv = img1(i,j);
-                
-                const T& saturation = hsv[1];
-                if (saturation < (double)(tollow / 100.0) || saturation > (double)(tolhigh / 100.0)) {
-                    hsv = VecT(0,0,0);
-                    continue;
-                }
-                
-                const T& hue = hsv[0];
-                if (hue < low || hue > high) {
-                    hsv = VecT(0,0,0);
-                }
-            }
-        }
-        
-        cv::cvtColor(img1, img1, CV_HSV2BGR);
-        cv::imshow("Output", img1);
-        
-        cout << "High: " << getTrackbarPos("High", "Output") << endl;
-        cout << "Low: " << getTrackbarPos("Low", "Output") << endl;
-        cout << "High tol: " << (double)getTrackbarPos("TolHigh", "Output") / 100.0 << endl;
-        cout << "Low tol: " << (double)getTrackbarPos("TolLow", "Output") / 100.0 << endl;
-        
-        int c = cvWaitKey(20);
-        if((char)c==27)
-            break;
-    }
-
-}
-
-void find_marker_in_HSV_image(string file) {
-    
-    // Values found using hsv_color_seperator_gui()
-    // For the big circle:
-//        High: 198
-//        Low: 157
-//        High tol: 0.52
-//        Low tol: 0.36
-    // For the small circle:
-//        High: 344
-//        Low: 314
-//        High tol: 0.66
-//        Low tol: 0.46
 }
 
 void animate_one() {
@@ -117,22 +42,59 @@ void animate_one() {
 
 void find_marker_from_gray_image(string file) {
     // Load image
-    Mat org = imread(file, 1);
-    Mat img = imread(file, 0);
+    Mat org = imread(file, 1); // Full color, to show the found marker.
+    Mat img = imread(file, 0); // Grayscale
     
     // Vector for found circles (Only one is found)
     vector<Vec3f> circles;
     
     // Blur the image and find the marker
     GaussianBlur(img, img, Size(7,7), 1.5, 1.5);
-    HoughCircles( img, circles, CV_HOUGH_GRADIENT, 2, 1000, 200, 100, 45, 50);
+    HoughCircles(img, circles, CV_HOUGH_GRADIENT, 2, 1000, 200, 100, 45, 50);
     
-    // Paint the marker
-    Point center(cvRound(circles[0][0]), cvRound(circles[0][1]));
-    int radius = cvRound(circles[0][2]);
-    circle( org, center, 3, Scalar(0,255,0), -1, 8, 0 );
-    circle( org, center, radius, Scalar(0,0,255), 3, 8, 0 );
+    // Have we found the big circle?
+    if (circles.size() > 0) {
+        
+        // Get the parameters for the big circle
+        Point big_center(cvRound(circles[0][0]), cvRound(circles[0][1]));
+        int big_radius = cvRound(circles[0][2]);
+        
+        // Split up the image in individual color planes.
+        vector<Mat> planes;
+        split(org, planes);
+        
+        // Black out the part of the image we do not need.
+        for (int x = 0; x < img.cols; x++) {
+            for (int y = 0; y < img.rows; y++) {
+                double d = sqrt(pow((double)x - (double)big_center.x, 2.0) + pow((double)y - (double)big_center.y, 2.0));
+                if (d > big_radius) {
+                    // Outside the circle
+                    planes[2].at<uchar>(y, x) = 0;
+                }
+            }
+        }
+        
+        // Find the small circle, don't use blur this time.
+        vector<Vec3f> circles2;
+        HoughCircles(planes[2], circles2, CV_HOUGH_GRADIENT, 1, 1000, 100, 2, 15, 18);
+        
+        // Did we find the small circle?
+        if (circles2.size() > 0) {
+            Point small_center(cvRound(circles2[0][0]), cvRound(circles2[0][1]));
+            circle( org, small_center, 3, Scalar(0,255,0), -1, 8, 0 );
+            circle( org, small_center, cvRound(circles2[0][2]), Scalar(255,0,0), 1, 8, 0 );
+            
+            // Print out the distance between the centers of the two circles.
+            double d = sqrt(pow((double)small_center.x - (double)big_center.x, 2.0) + pow((double)small_center.y - (double)big_center.y, 2.0));
+            cout << "Distance: \t" << d << endl;
+            
+        }
+        
+        circle( org, big_center, 3, Scalar(0,255,0), -1, 8, 0 );
+        circle( org, big_center, big_radius, Scalar(0,0,255), 1, 8, 0 );
+    }
     
-    imshow( "circles", org);
+    // Show the current image with marker marked.
+    imshow("circles", org);
     waitKey(10);
 }
